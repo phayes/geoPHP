91a3d53878a7565c6f3507ba5c20d5a9afe9eb68
Patrick Hayes <patrick.d.hayes@gmail.com>
2011-03-11 18:52:52 +0000

    Removing redundant includes

--- lib/GeometryLoader.class.php
+++ lib/GeometryLoader.class.php
@@ -1,22 +1,5 @@
 <?php
 
-include_once("lib/GeoJSON/GeoJSON.class.php");
-include_once("lib/GeoJSON/adapters/GeoJSON_Adapter.class.php");
-include_once("lib/GeoJSON/adapters/z_dependant/GeoJSON_Adapter_Doctrine.class.php");
-include_once("lib/GeoJSON/WKT/WKT.class.php");
-include_once("lib/GeoJSON/lib/Feature.class.php");
-include_once("lib/GeoJSON/lib/FeatureCollection.class.php");
-include_once("lib/GeoJSON/lib/Geometry/Geometry.class.php");
-include_once("lib/GeoJSON/lib/Geometry/Point.class.php");
-include_once("lib/GeoJSON/lib/Geometry/Collection.class.php");
-include_once("lib/GeoJSON/lib/Geometry/LineString.class.php");
-include_once("lib/GeoJSON/lib/Geometry/MultiPoint.class.php");
-include_once("lib/GeoJSON/lib/Geometry/LinearRing.class.php");
-include_once("lib/GeoJSON/lib/Geometry/Polygon.class.php");
-include_once("lib/GeoJSON/lib/Geometry/MultiLineString.class.php");
-include_once("lib/GeoJSON/lib/Geometry/MultiPolygon.class.php");
-include_once("lib/GeoJSON/lib/Geometry/GeometryCollection.class.php");
-
 class GeometryLoader {
   
   function load($data, $type = 'autodetect') {
