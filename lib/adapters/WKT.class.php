4b3051722f7e28aaa4f87d6e3ec58fed4223532d
Patrick Hayes <patrick.d.hayes@gmail.com>
2012-03-03 01:48:26 -0800

    Removing camptocamp code from WKT parser

--- lib/adapters/WKT.class.php
+++ lib/adapters/WKT.class.php
@@ -20,31 +20,17 @@
  */
 class WKT extends GeoAdapter
 {
-
-  private $regExes = array(
-    'typeStr'               => '/^\s*(\w+)\s*\(\s*(.*)\s*\)\s*$/',
-    'spaces'                => '/\s+/',
-    'parenComma'            => '/\)\s*,\s*\(/',
-    'doubleParenComma'      => '/\)\s*\)\s*,\s*\(\s*\(/',
-  );
-
-  const POINT               = 'point';
-  const MULTIPOINT          = 'multipoint';
-  const LINESTRING          = 'linestring';
-  const MULTILINESTRING     = 'multilinestring';
-  const POLYGON             = 'polygon';
-  const MULTIPOLYGON        = 'multipolygon';
-  const GEOMETRYCOLLECTION  = 'geometrycollection';
-
+  
   /**
    * Read WKT string into geometry objects
    *
    * @param string $WKT A WKT string
    *
-   * @return Geometry|GeometryCollection
+   * @return Geometry
    */
   public function read($wkt) {
-    $wkt = strval($wkt);
+    $wkt = trim($wkt);
+    $wkt = str_replace(', ', ',', $wkt);
     
     // If geos is installed, then we take a shortcut and let it parse the WKT
     if (geoPHP::geosInstalled()) {
@@ -52,14 +38,123 @@ class WKT extends GeoAdapter
       return geoPHP::geosToGeometry($reader->read($wkt));
     }
     
-    $matches = array();
-    if (!preg_match($this->regExes['typeStr'], $wkt, $matches)) {
-      return null;
+    // For each geometry type, check to see if we have a match at the
+    // beggining of the string. If we do, then parse using that type
+    foreach (geoPHP::geometryList() as $geom_type) {
+      $wkt_geom = strtoupper($geom_type);
+      if (strtoupper(substr($wkt, 0, strlen($wkt_geom))) == $wkt_geom) {
+        $data_string = $this->getDataString($wkt, $wkt_geom);
+        $method = 'parse'.$geom_type;
+        return $this->$method($data_string);
+      }
     }
-    
-    return $this->parse(strtolower($matches[1]), $matches[2]);
   }
   
+  private function parsePoint($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode(' ',$data_string);
+    return new Point($parts[0], $parts[1]);
+  }
+
+  private function parseLineString($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode(',',$data_string);
+    $points = array();
+    foreach ($parts as $part) {
+      $points[] = $this->parsePoint($part);
+    }
+    return new LineString($points);
+  }
+
+  private function parsePolygon($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode('),(',$data_string);
+    $lines = array();
+    foreach ($parts as $part) {
+      if (!$this->beginsWith($part,'(')) $part = '(' . $part;
+      if (!$this->endsWith($part,')'))   $part = $part . ')';
+      $lines[] = $this->parseLineString($part);
+    }
+    return new Polygon($lines);
+  }
+
+  private function parseMultiPoint($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode(',',$data_string);
+    $points = array();
+    foreach ($parts as $part) {
+      $points[] = $this->parsePoint($part);
+    }
+    return new MultiPoint($points);
+  }
+  
+  private function parseMultiLineString($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode('),(',$data_string);
+    $lines = array();
+    foreach ($parts as $part) {
+      // Repair the string if the explode broke it
+      if (!$this->beginsWith($part,'(')) $part = '(' . $part;
+      if (!$this->endsWith($part,')'))   $part = $part . ')';
+      $lines[] = $this->parseLineString($part);
+    }
+    return new MultiLineString($lines);
+  }
+
+  private function parseMultiPolygon($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $parts = explode(')),((',$data_string);
+    $polys = array();
+    foreach ($parts as $part) {
+      // Repair the string if the explode broke it
+      if (!$this->beginsWith($part,'((')) $part = '((' . $part;
+      if (!$this->endsWith($part,'))'))   $part = $part . '))';
+      $polys[] = $this->parsePolygon($part);
+    }
+    return new MultiPolygon($polys);
+  }
+
+  private function parseGeometryCollection($data_string) {
+    $data_string = $this->trimParens($data_string);
+    $geometries = array();
+    $matches = array();
+    $data_pattern = "( |,|[0-9]|\.|-|\)|\()+";
+    $pattern = "/[A-Z]+$data_pattern/";
+    preg_match_all($pattern, $data_string, $matches);
+    
+    foreach ($matches[0] as $item) {
+      $geometries[] = $this->read(trim($item, ','));
+    }
+    return new GeometryCollection($geometries);
+  }
+
+  protected function getDataString($wkt, $type) {
+    return substr($wkt, strlen($type));
+  }
+  
+  /**
+   * Trim the parenthesis and spaces
+   */
+  protected function trimParens($str) {
+    $str = trim($str);
+    
+    // We want to only strip off one set of parenthesis
+    if ($this->beginsWith($str, '(')) {
+      return substr($str,1,-1);
+    }
+    else return $str;
+  }
+  
+  protected function beginsWith($str, $char) {
+    if (substr($str,0,strlen($char)) == $char) return TRUE;
+    else return FALSE;
+  }
+
+  protected function endsWith($str, $char) {
+    if (substr($str,(0 - strlen($char))) == $char) return TRUE;
+    else return FALSE;
+  }
+    
   /**
    * Serialize geometries into a WKT string.
    *
@@ -81,92 +176,6 @@ class WKT extends GeoAdapter
     }
     
     return strtoupper($type).' ('.$data.')';
-  }
-
-  /**
-   * Parse WKT string into geometry objects
-   *
-   * @param string $WKT A WKT string
-   *
-   * @return Geometry|GeometryCollection
-   */
-  public function parse($type, $str) {
-    $matches = array();
-    $components = array();
-
-    switch ($type) {
-      case self::POINT:
-        $coords = $this->pregExplode('spaces', $str);
-        return new Point(floatval($coords[0]), floatval($coords[1]));
-
-      case self::MULTIPOINT:
-        $points = $this->pregExplode('parenComma', $str);
-        foreach ($points as $p) {
-          $point = $this->trimParens($p);
-          $components[] = $this->parse(self::POINT, $point);
-        }
-        return new MultiPoint($components);
-
-      case self::LINESTRING:
-        foreach (explode(',', trim($str)) as $point) {
-          $components[] = $this->parse(self::POINT, $point);
-        }
-        return new LineString($components);
-
-      case self::MULTILINESTRING:
-        $lines = $this->pregExplode('parenComma', $str);
-        foreach ($lines as $l) {
-          $line = $this->trimParens($l);
-          $components[] = $this->parse(self::LINESTRING, $line);
-        }
-        return new MultiLineString($components);
-
-      case self::POLYGON:
-        $rings= $this->pregExplode('parenComma', $str);
-        foreach ($rings as $r) {
-          $ring = $this->trimParens($r);
-          $linestring = $this->parse(self::LINESTRING, $ring);
-          $components[] = new LineString($linestring->getComponents());
-        }
-        return new Polygon($components);
-
-      case self::MULTIPOLYGON:
-        $polygons = $this->pregExplode('doubleParenComma', $str);
-        foreach ($polygons as $p) {
-          $polygon = $this->trimParens($p);
-          $components[] = $this->parse(self::POLYGON, $polygon);
-        }
-        return new MultiPolygon($components);
-
-      case self::GEOMETRYCOLLECTION:
-        $str = preg_replace('/,\s*([A-Za-z])/', '|$1', $str);
-        $wktArray = explode('|', trim($str));
-        foreach ($wktArray as $wkt) {
-          $components[] = $this->read($wkt);
-        }
-        return new GeometryCollection($components);
-
-      default:
-        return null;
-    }
-  }
-  
-  /**
-   * Trim the parenthesis 
-   *
-   */
-  protected function trimParens($str) {
-    return trim($str,'() ');
-  }
-  
-  /**
-   * Split string according to first match of passed regEx index of $regExes
-   *
-   */
-  protected function pregExplode($regEx, $str) {
-    $matches = array();
-    preg_match($this->regExes[$regEx], $str, $matches);
-    return empty($matches)?array(trim($str)):explode($matches[0], trim($str));
   }
   
   /**
