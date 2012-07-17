87b036d5e9e582adb8cb13093ce9709fde49680a
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-07-17 16:31:53 +0200

    Updates to ElevationMetadataProvider.

--- lib/metadata/ElevationMetadataProvider.class.php
+++ lib/metadata/ElevationMetadataProvider.class.php
@@ -2,7 +2,7 @@
 
 class ElevationMetadataProvider implements MetadataProvider {
 
-  public $capabilities = array('ele');
+  public $capabilities = array('ele', 'maxEle', 'minEle');
 
   public function provides($key) {
     if (in_array($key, $this->capabilities)) {return TRUE;};
@@ -10,9 +10,53 @@ class ElevationMetadataProvider implements MetadataProvider {
   }
 
   public function get($target, $key, $options) {
+
+    if ($target instanceof LineString) {
+      if ($key === 'maxEle') {
+        $max = NULL;
+        foreach ($target->components as $component) {
+          if ($component->getMetadata('ele') > $max || is_null($max)) {
+            $max = $component->getMetadata('ele');
+          }
+        }
+        return $max;
+      }
+      if ($key === 'minEle') {
+        $min = NULL;
+        foreach ($target->components as $component) {
+          if ($component->getMetadata('ele') < $min || is_null($min)) {
+            $min = $component->getMetadata('ele');
+          }
+        }
+        return $min;
+      }
+    }
+
+    if ($target instanceof MultiLineString) {
+      if ($key === 'maxEle') {
+        $max = NULL;
+        foreach ($target->components as $component) {
+          if ($component->getMetadata($key) > $max || is_null($max)) {
+            $max = $component->getMetadata($key);
+          }
+        }
+        return $max;
+      }
+      if ($key === 'minEle') {
+        $min = NULL;
+        foreach ($target->components as $component) {
+          if ($component->getMetadata($key) < $min || is_null($min)) {
+            $min = $component->getMetadata($key);
+          }
+        }
+        return $min;
+      }
+    }
+
     if ($this->provides($key)) {
       return $target->metadata['metadatas'][__CLASS__][$key];
     }
+
   }
 
   public function set($target, $key, $value) {
