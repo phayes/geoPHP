7ea68ede95cc86c0bfea99d94ed57596fe01422a
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-07-22 10:58:13 +0200

    New adapters tests. Fix some stuff in aliases, geos and placeholders tests. Enabled test file autodetection in tests.php

--- tests/tests.php
+++ tests/tests.php
@@ -3,12 +3,9 @@ require_once('simpletest/autorun.php');
 require_once('../geoPHP.inc');
 
 class AllTests extends TestSuite {
-
-  function allTests() {
-    $this->TestSuite('All tests');
-    $this->addFile('tests/methods.test');
-    //$this->addFile('tests/aliases.test');
-    //$this->addFile('tests/geos.test');
-    //$this->addFile('tests/placeholders.test');
+  function __construct() {
+    parent::__construct();
+    $this->collect(dirname(__FILE__) . '/tests',
+      new SimplePatternCollector('/.test/'));
   }
 }
