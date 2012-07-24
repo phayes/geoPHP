7b9433acf1d5072b77ee025ee0d70a8fc3630b83
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-07-24 12:47:46 +0200

    Update Duration Metadata Provider, more speed.

--- lib/metadata/DurationMetadataProvider.class.php
+++ lib/metadata/DurationMetadataProvider.class.php
@@ -10,10 +10,14 @@ class DurationMetadataProvider implements MetadataProvider {
   }
 
   public function get($target, $key, $options) {
+    if ($this->isAvailable($target, $key)) {
+      return $target->metadata['metadatas'][__CLASS__][$key];
+    }
+
     if (!$this->provides($key)) {return FALSE;}
 
     if ($key == 'durations') {
-      if (!isset($target->metadata['metadatas'][__CLASS__]['durations'])) {
+      if ($target instanceof LineString) {
         $points = $target->getPoints();
         $count = count($points);
         for($i=0; $i<$count-1; $i++) {
@@ -23,30 +27,41 @@ class DurationMetadataProvider implements MetadataProvider {
           $durations[] = abs(strtotime($current->getMetadata('time')) - strtotime($next->getMetadata('time')));
         }
         $this->set($target, 'durations', $durations);
-        return $durations;
-      } else {
-        return $target->metadata['metadatas'][__CLASS__]['durations'];
+      }
+      if ($target instanceof MultiLineString) {
+        $durations = array();
+        foreach ($target->components as $component) {
+          $durations = array_merge($durations, $this->get($component, 'durations', $options));
+        }
+        $this->set($target, 'durations', $durations);
       }
     }
 
     if ($key == 'stopDurations') {
-      if (!isset($target->metadata['metadatas'][__CLASS__]['stopDurations'])) {
+      if ($target instanceof LineString) {
         $points = $target->getPoints();
         $count = count($points);
+        $durations = array();
         for($i=0; $i<$count-1; $i++) {
           $current = $points[$i];
           $next = $points[$i+1];
           if (!($next instanceof Point)) {return 0;}
           $linestring = new LineString(array($current, $next));
           $duration = abs(strtotime($current->getMetadata('time')) - strtotime($next->getMetadata('time')));
-          if ($linestring->greatCircleLength() < $options['threshold']) {
+          $length = $linestring->greatCircleLength();
+          if ($length < $options['threshold']) {
             $durations[] = $duration;
           }
+
         }
         $this->set($target, 'stopDurations', $durations);
-        return $durations;
-      } else {
-        return $target->metadata['metadatas'][__CLASS__]['stopDurations'];
+      }
+      if ($target instanceof MultiLineString) {
+        $durations = array();
+        foreach ($target->components as $component) {
+          $durations = array_merge($durations, $this->get($component, 'stopDurations', $options));
+        }
+        $this->set($target, 'stopDurations', $durations);
       }
     }
 
@@ -74,6 +89,18 @@ class DurationMetadataProvider implements MetadataProvider {
       return TRUE;
     }
     return FALSE;
+  }
+
+  public function isAvailable($target, $keys) {
+    if (!is_array($keys)) {
+      $keys = (array) $keys;
+    }
+    foreach ($keys as $key) {
+      if (!isset($target->metadata['metadatas'][__CLASS__][$key])) {
+        return FALSE;
+      }
+    }
+    return TRUE;
   }
 
   public function id() {
