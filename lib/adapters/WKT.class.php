f88da38b09f61d76d510149dc1d6dac1d2c227cd
mkdgs <mickael.desgranges@gmail.com>
2012-08-31 11:46:09 +0200

    - fix Notice: Undefined variable: str

--- lib/adapters/WKT.class.php
+++ lib/adapters/WKT.class.php
@@ -25,14 +25,12 @@ class WKT extends GeoAdapter
     }
     
     // If geos is installed, then we take a shortcut and let it parse the WKT
-    if (geoPHP::geosInstalled()) {
+    if ( geoPHP::geosInstalled() ) {
       $reader = new GEOSWKTReader();      
       $geom = geoPHP::geosToGeometry($reader->read($wkt));
       if ($srid) $geom->setSRID($srid);
       return $geom;      
     }
-
-    //$wkt = str_replace(', ', ',', $wkt);    why ?
     
     $geoTypeList = geoPHP::geometryList();
     // geometry type is the first word    
@@ -46,40 +44,17 @@ class WKT extends GeoAdapter
     		return $geom;
     	}
     } 
-    		
-    /*		
-    // For each geometry type, check to see if we have a match at the
-    // beggining of the string. If we do, then parse using that type
-    foreach (geoPHP::geometryList() as $geom_type) {
-      $wkt_geom = strtoupper($geom_type);
-      if (strtoupper(substr($wkt, 0, strlen($wkt_geom))) == $wkt_geom) {
-        $data_string = $this->getDataString($wkt, $wkt_geom);
-        $method = 'parse'.$geom_type;
-        
-        $geom = $this->$method($data_string);
-        if ($srid)       $geom->setSRID($srid);
-        return $geom;        
-      }
-    }
-    */
   }
   
   private function parsePoint($data_string, $hasZ=null, $hasM=null) {
-    $data_string = $this->trimParens($data_string);
-    $parts = explode(' ',$data_string);
+    $parts = explode(' ', trim($data_string));
     $z = $m = null;
-    if ( $hasZ ) {
-    	$z = $parts[2];
-    }
-    if ( $hasM ) {
-    	$m = ($hasZ) ? $parts[3] :  $parts[2];
-    }
+    if ( $hasZ ) $z = $parts[2];
+    if ( $hasM ) $m = ($hasZ) ? $parts[3] :  $parts[2];
     return new Point($parts[0], $parts[1], $z, $m);
   }
 
   private function parseLineString($data_string) {
-    $data_string = $this->trimParens($data_string);
-
     // If it's marked as empty, then return an empty line
     if ($data_string == 'EMPTY') return new LineString();
     
@@ -91,25 +66,23 @@ class WKT extends GeoAdapter
     return new LineString($points);
   }
 
-  private function parsePolygon($data_string) {
-    $data_string = $this->trimParens($data_string);
-    
+  private function parsePolygon($data_string) {   
     // If it's marked as empty, then return an empty polygon
     if ($data_string == 'EMPTY') return new Polygon();
     
-    $parts = explode('),(',$data_string);
+    //$parts = explode('),',$data_string);
+    // avoid mistake with blank space
     $lines = array();
-    foreach ($parts as $part) {
-      if (!$this->beginsWith($part,'(')) $part = '(' . $part;
-      if (!$this->endsWith($part,')'))   $part = $part . ')';
-      $lines[] = $this->parseLineString($part);
-    }
+    if (  preg_match_all('/\(([^)(]*)\)/', $data_string, $m) ) {
+    	$parts = $m[1];    	
+    	foreach ($parts as $part) {
+    		$lines[] = $this->parseLineString($part);
+    	}
+    }   
     return new Polygon($lines);
   }
 
   private function parseMultiPoint($data_string) {
-    $data_string = $this->trimParens($data_string);
-    
     // If it's marked as empty, then return an empty MutiPoint
     if ($data_string == 'EMPTY') return new MultiPoint();
     
@@ -122,88 +95,53 @@ class WKT extends GeoAdapter
   }
   
   private function parseMultiLineString($data_string) {
-    $data_string = $this->trimParens($data_string);
-
     // If it's marked as empty, then return an empty multi-linestring
     if ($data_string == 'EMPTY') return new MultiLineString();
     
     $parts = explode('),',$data_string);
     $lines = array();
-    foreach ($parts as $part) {
-      //var_dump($part);
-      // Repair the string if the explode broke it
-      //if (!$this->beginsWith($part,'(')) $part = '(' . $part;
-      // if (!$this->endsWith($part,')'))   $part = $part . ')';
-      $lines[] = $this->parseLineString($part);
+    foreach ($parts as $part) {     
+      $lines[] = $this->parseLineString(trim($part,')( '));
     }
     return new MultiLineString($lines);
   }
 
   private function parseMultiPolygon($data_string) {
-    $data_string = $this->trimParens($data_string);
-
     // If it's marked as empty, then return an empty multi-polygon
     if ($data_string == 'EMPTY') return new MultiPolygon();
     
-    $parts = explode(')),((',$data_string);
+    $parts = explode(')),',$data_string);
     $polys = array();
-    foreach ($parts as $part) {
-      // Repair the string if the explode broke it
-      if (!$this->beginsWith($part,'((')) $part = '((' . $part;
-      if (!$this->endsWith($part,'))'))   $part = $part . '))';
+    foreach ($parts as $part) {     
+      $parts = '(('.trim($part,')( ').'))';	
       $polys[] = $this->parsePolygon($part);
     }
     return new MultiPolygon($polys);
   }
 
   private function parseGeometryCollection($data_string) {
-    $data_string = $this->trimParens($data_string);
-
-    // If it's marked as empty, then return an empty geom-collection
+     // If it's marked as empty, then return an empty geom-collection
     if ($data_string == 'EMPTY') return new GeometryCollection();
     
     $geometries = array();
-    $matches = array();
-    $str = preg_replace('/,\s*([A-Za-z])/', '|$1', $data_string);
-    $components = explode('|', trim($str));
-    
+    // do not cut ZM 
+    $str = preg_replace('/([a-z]{3,})/i', '|$1', $data_string);
+    $components = explode('|', substr($str,1,-1) );
+
     foreach ($components as $component) {
-      $geometries[] = $this->read($component);
+      $geometries[] = $this->read(trim($component,', '));
     }
     return new GeometryCollection($geometries);
   }
 
   protected function getDataString($wkt, $type) {
-    // data is between () or is empty
-    if ( preg_match('#(z{0,1})(m{0,1})[\s]*(\([0-9\.,\s)(]*\))#i', $wkt, $m) ) {
+    // data is between () or is empty  	
+    if ( preg_match('#(z{0,1})(m{0,1})[\s]*\((.*)\)$#i', trim($wkt), $m) ) {
   		return array($m[1], $m[2], $m[3]);
   	}
-  	else return 'EMPTY';
-    //return substr($wkt, strlen($type));
-  }
-  
-  /**
-   * Trim the parenthesis and spaces
-   */
-  protected function trimParens($str) {
-    $str = trim($str);
-    
-    // We want to only strip off one set of parenthesis
-    if ($this->beginsWith($str, '(')) {
-      return substr($str,1,-1);
-    }
-    else return $str;
-  }
-  
-  protected function beginsWith($str, $char) {
-    if (substr($str,0,strlen($char)) == $char) return TRUE;
-    else return FALSE;
+  	return 'EMPTY';
   }
 
-  protected function endsWith($str, $char) {
-    if (substr($str,(0 - strlen($char))) == $char) return TRUE;
-    else return FALSE;
-  }
     
   /**
    * Serialize geometries into a WKT string.
