73a69fb3ec49c11fae70167e9aae26fd536efbe9
mprins <mprins@users.sf.net>
2014-08-29 13:51:05 +0200

    fix typo

--- lib/adapters/KML.class.php
+++ lib/adapters/KML.class.php
@@ -50,12 +50,11 @@ class KML extends GeoAdapter
   }
 
   public function geomFromText($text) {
-
     // Change to lower-case and strip all CDATA
     $text = mb_strtolower($text, mb_detect_encoding($text));
     $text = preg_replace('/<!\[cdata\[(.*?)\]\]>/s','',$text);
 
-    // Load into DOMDOcument
+    // Load into DOMDocument
     $xmlobj = new DOMDocument();
     @$xmlobj->loadXML($text);
     if ($xmlobj === false) {
