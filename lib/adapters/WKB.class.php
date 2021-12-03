8dc6bce50184b7a3ffe2afc592f585ec5534d6f6
Timothy Winters <t.winters099@gmail.com>
2021-12-03 00:00:54 -0500

    Fix empty point binary output representation

--- lib/adapters/WKB.class.php
+++ lib/adapters/WKB.class.php
@@ -202,13 +202,11 @@ class WKB extends GeoAdapter
   }
 
   function writePoint($point) {
-    // Set the coords
-    if (!$point->isEmpty()) {
-      $wkb = pack('dd',$point->x(), $point->y());
-      return $wkb;
-    } else {
-      return '';
+    if ($point->isEmpty()) {
+      return pack('dd', NAN, NAN);
     }
+
+    return pack('dd', $point->x(), $point->y());
   }
 
   function writeLineString($line) {
